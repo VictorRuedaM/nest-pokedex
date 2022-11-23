853f1298056e4ad48e6ca41c9f18f8ae07ef3a17
Victor Rueda <vialruma1985@gmail.com>
2022-11-23 18:06:20 -0500

    Insert pokemons into DB - testing

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -14,6 +14,9 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
       
       
     }])
+  ],
+  exports: [
+    MongooseModule,
   ]
 })
 export class PokemonModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 
 @Injectable()
@@ -8,19 +11,27 @@ export class SeedService {
   
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
+
   async executeSeed() {
 
     const {data} = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=10`);
 
-    data.results.forEach(({name, url}) => {
+    data.results.forEach(async({name, url}) => {
 
       const segments = url.split('/');
       const np = +segments[segments.length -2];
 
+      const pokemon = await this.pokemonModel.create({name, np})
+
       console.log(name, np)
     })
 
-    return data.results
+    return `Seed executed`
     
   }
 
